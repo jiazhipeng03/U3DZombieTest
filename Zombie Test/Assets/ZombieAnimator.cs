8c8336cad361ba2ecb676816c4abb5091405c778
jiazhipeng03 <jiazhipeng03@163.com>
2015-07-16 01:17:33 +0800

    first version
    Contents before animation controllers have been finished.
    Next I need to add animation events.

---
+++ Zombie Test/Assets/ZombieAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ZombieAnimator : MonoBehaviour {
+	public float framesPerSecond;
+	public Sprite[] sprites;
+	private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites[ index ];
+	}
+}
